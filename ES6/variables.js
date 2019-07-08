198ae9bc11458d3c08b26aa33b93ddfbd62b1d3a
djprano <djprano@gmail.com>
2019-07-08 13:43:24 +0200

    configuracion del debug

---
+++ ES6/variables.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+var x
+
+
+//ES6
+
+let global = ''
+
+function algo(){
+    let local_var
+    {
+        let localbloque
+    }
+}
+
+const z = 0
+
+//diferencia entre elementales y referencias
+
+'Pepe'
+23
+true
+undefined
+{}
+
+const o = {}
+o.nombre = 'jose luis'
